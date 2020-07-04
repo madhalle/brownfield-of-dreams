e52e2adbe4a5851550185f5ed208176812a70be7
Mad Hallé <mad.halle@icloud.com>
2020-07-04 13:31:10 -0600

    oauth working

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,9 +2,15 @@ class AuthenticationController < ApplicationController
 
   def create
     auth = request.env['omniauth.auth']
-    current_user.token = auth['omniauth.auth']['credentials']['token']
-    current_user.save
+    # current_user.token = auth['credentials']['token']
+    # require "pry"; binding.pry
+    current_user.update_attributes!(token: auth['credentials']['token'])
     redirect_to dashboard_path
   end
+
+  # private
+  # def permitted_params
+  #   params.permit(:user => [:username, :email])
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
+  validates :password, presence: true, on: :update, allow_blank: true
   validates_presence_of :first_name
   enum role: { default: 0, admin: 1 }
   has_secure_password
+  # has_secure_password(validations: false)
 
 end
