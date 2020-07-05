411c508569d11bcea92f73afb37358a466b69859
Jack Cullen <jpcullen20@gmail.com>
2020-07-05 16:10:53 -0600

    Add validations to tutorial model

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -3,4 +3,7 @@ class Tutorial < ApplicationRecord
   # , -> { order(position: :ASC) }, inverse_of: :tutorial
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :thumbnail, presence: true
 end
