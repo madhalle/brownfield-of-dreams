7fa62e250101264b752abcf8249247235f968d03
Jack Cullen <jpcullen20@gmail.com>
2020-07-06 15:05:57 -0600

    Add github username to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_205754) do
+ActiveRecord::Schema.define(version: 2020_07_06_204736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_06_30_205754) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "token"
+    t.string "github_username"
     t.index ["email"], name: "index_users_on_email"
   end
 
---
+++ spec/features/user/user_can_add_a_friend_spec.rb
@@ -0,0 +1,32 @@
+# Background: A user (Josh) exists in the system with a Github token.
+# The user has two followers on Github. One follower (Dione) also has an account
+# within our database. The other follower (Mike) does not have an account in our system.
+# If a follower or following has an account in our system we want to include a link next
+# to their name to allow us to add as a friend.
+#
+# In this case Dione would have an Add as Friend link next to her name. Mike would not have the link next to his name.
+require 'rails_helper'
+
+RSpec.describe "When visiting the user dashboard" do
+  before :each do
+    @user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    # @user1 = create(:user, token: ENV['GITHUB_TOKEN'])
+    # @user2 = create(:user, token: ENV['GITHUB_TOKEN_2'])
+  end
+
+  it "users have Github usernames" do
+    visit "/dashboard"
+    click_link "Connect to Github"
+
+    expect(current_path).to eq dashboard_path
+    expect(page).to have_css('.github')
+    expect(page).to_not have_link('Connect to Github')
+    expect(@user.github_username).to eq('jpc20')
+  end
+
+  xit "Users see a link to add friend next to followers with an account" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+
+  end
+end
