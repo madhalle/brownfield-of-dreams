ba63016d97131b72af6192bbc374c812cec8849f
Jack Cullen <jpcullen20@gmail.com>
2020-07-08 16:41:43 -0600

    Add tutorial method to hide classroom content

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    @tutorials = if params[:tag]
+    @tutorials = if params[:tag] && current_user
                    Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
-                 else
+                 elsif current_user
                    Tutorial.all.paginate(page: params[:page], per_page: 5)
+                 else
+                   Tutorial.hide_classroom_content.paginate(page: params[:page], per_page: 5)
                  end
   end
 end
